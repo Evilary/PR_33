5f2cdc98d12b3ff85d2aad7821446b1374cb9821
df275 <df275@EVIL>
2026-05-17 19:39:29 +0500

    Разработанные вспомогательные классы для сервера. Чернышков.

---
+++ KeyPass_Chernyshkov/Classes/DatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using KeyPass_Chernyshkov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeyPass_Chernyshkov.Classes
+{
+    public class DatabaseManager : DbContext
+    {
+        public DbSet<Storage> Storages { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseManager() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;uid=student;pwd=;database=Storage;",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
