4ce8f8aaa00e3ce969a911b2e2e7592be6e42993
df275 <df275@EVIL>
2026-05-17 18:40:44 +0500

    Подключение Swagger для сервера. Чернышков

--- KeyPass_Chernyshkov/Program.cs
+++ KeyPass_Chernyshkov/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddMvc(option => option.EnableEndpointRouting = true);
+builder.Services.AddSwaggerGen(option => {
+    option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Инструментарий"
+    });
 
-app.MapGet("/", () => "Hello World!");
+    string PathFile = Path.Combine(AppContext.BaseDirectory, "KeyPass_Chernyshkov.xml");
+    option.IncludeXmlComments(PathFile);
+});
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Инструментарий");
+});
 
 app.Run();
