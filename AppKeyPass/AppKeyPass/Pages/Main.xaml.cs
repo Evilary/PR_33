15f10640261c68c585617590cd617189f3b8ea77
df275 <df275@EVIL>
2026-05-17 21:25:45 +0500

    Разработанные страницы приложения для клиента. Чернышков.

---
+++ AppKeyPass/AppKeyPass/Pages/Main.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using AppKeyPass.Contexts;
+using AppKeyPass.Models;
+
+namespace AppKeyPass.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            GetStorage();
+        }
+
+        public async Task GetStorage()
+        {
+            List<Storage> Storages = await StorageContext.Get();
+            StorageList.Children.Clear();
+            foreach (Storage Storage in Storages)
+            {
+                StorageList.Children.Add(new Elements.Item(Storage, this));
+            }
+        }
+
+        private void OpenPageAdd(object sender, System.Windows.RoutedEventArgs e) =>
+            MainWindow.Init.OpenPages(new Pages.Add());
+    }
+}
